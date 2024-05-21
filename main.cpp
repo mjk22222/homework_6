6b4fa2a5d5a28a5be382f134eb29445a1a903d86
mjk22222 <maxgerasimenko221@gmail.com>
2024-05-21 22:49:07 +0200

    added

---
+++ main.cpp
@@ -0,0 +1,17 @@
+﻿#include <SFML/Graphics.hpp>
+#include <Circle.hpp>
+#include <Game.hpp>
+
+int main()
+{
+    mt::Game game(1100, 600, "Game!");
+
+    if (!game.Setup(10)) {
+        return -1;
+    };
+
+    game.LifeCycle();
+
+
+    return 0;
+}
